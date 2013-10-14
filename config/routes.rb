603e299468018ec71d19fc115ce4e7c983344168
Ray-Mice <princemice@126.com>
2013-10-14 14:55:16 +0800

    Add three simple static pages and relative tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Kulu::Application.routes.draw do
-  get 'pages/home'
-  get 'pages/contact'
-  get 'pages/about'
+  root to: 'pages#home'
+  match '/contact', to: 'pages#contact', via: 'get'
+  match '/about', to: 'pages#about', via: 'get'
+  # match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  subject { page }
   describe "Home page" do
-    it "should have the content 'Historical Battle'" do
-      visit '/pages/home'
-      expect(page).to have_content('Historical Battle')
-    end
+      before { visit root_path }
+      it { should have_title('Historical Battle') }
+      it { should_not have_title(full_title('home')) }
+  end
+
+  describe "About page" do
+      before { visit about_path }
+      it { should have_title('Historical Battle') }
+      it { should have_content('About Me') }
+      it { should have_title(full_title('About Me')) }
+  end
+
+  describe "Contact page" do
+      before { visit contact_path }
+      it { should have_title('Historical Battle') }
+      it { should have_content('Contact Me') }
+      it { should have_title(full_title('Contact Me')) }
   end
 end
